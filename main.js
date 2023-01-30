e2054c5e843867c5b0107e6729077f1801e21d84
Giygas <gustavorossich@gmail.com>
2023-01-31 00:05:37 +0100

    added base buttons

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 import '@picocss/pico/scss/pico.scss'
 import './style.scss'
 
-import plusIcon from './plusIcon.svg'
+import plusIcon from './icons/plusIcon.svg'
+import refreshIcon from './icons/refreshIcon.svg'
+import arrowDown from './icons/arrowDown.svg'
 
 document.querySelector('#app').innerHTML = `
-  <div class="game">
+  <div class="options">
     <h2>
-      <span id="newGame">
+      <span id="newGame" class="button">
         <img src=${plusIcon} alt="Plus"/>NEW GAME
+      </span>
+    </h2>
+    
+    <div class="dice"></div>
+    
+    <h2 class="spacer">
+      <span id="roll" class="button">
+        <img src=${refreshIcon} alt="Plus"/>ROLL
+      </span>
+    </h2>
+    
+    <h2>
+      <span id="hold" class="button">
+        <img src=${arrowDown} alt="Plus"/>HOLD
       </span>
     </h2>
   </div>
