16e77b151642fc14a089cb2d086f33a79576783b
Giygas <gustavorossich@gmail.com>
2023-01-30 21:23:48 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+import './style.scss'
+import '@picocss/pico/scss/pico.scss'
+// import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div class="player p1">
+    asdf
+  </div>
+  <div class="player p2">
+    asdf
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
