d3bd67e97a2652e64147735ce0e39ba4f22aa23c
Giygas <gustavorossich@gmail.com>
2023-01-30 23:28:09 +0100

    Added new game button

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 import '@picocss/pico/scss/pico.scss'
 import './style.scss'
-// import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+
+import plusIcon from './plusIcon.svg'
 
 document.querySelector('#app').innerHTML = `
+  <div class="game">
+    <h2>
+      <span id="newGame">
+        <img src=${plusIcon} alt="Plus"/>NEW GAME
+      </span>
+    </h2>
+  </div>
+  
   <div class="player p1">
-    asdf
+    
   </div>
   <div class="player p2">
-    asdf
+    
   </div>
 `
 
