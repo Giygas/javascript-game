52d74ecdfeb558b855c0ec5f37d91af9640b097e
Giygas <gustavorossich@gmail.com>
2023-01-31 22:38:07 +0100

    Added vue components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
 </script>
 
 <template>
@@ -7,7 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <div class="grid newGame">
             <h2>
               <span id="newGame" class="button">
-                <img src=${plusIcon} alt="Plus"/>NEW GAME
+                <img src="./assets/plusIcon.svg" alt="Plus"/>NEW GAME
               </span>
             </h2>
         </div>
@@ -27,12 +27,12 @@ import HelloWorld from './components/HelloWorld.vue'
           <div class="buttonWrapper">
             <h2 class="spacer button">
               <span id="roll" class="button">
-                <img src=${refreshIcon} alt="Plus"/>ROLL
+                <img src="./assets/refreshIcon.svg" alt="Plus"/>ROLL
               </span>
             </h2>
             <h2>
               <span id="hold" class="button">
-                <img src=${arrowDown} alt="Plus"/>HOLD
+                <img src="./assets/arrowDown.svg" alt="Plus"/>HOLD
               </span>
             </h2>
           </div>
@@ -46,16 +46,79 @@ import HelloWorld from './components/HelloWorld.vue'
       <div class="player p2"></div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style lang="scss" scoped>
+.main-container {
+  background-color: #ffffff;
+  position: absolute;
+  padding-top: 3rem;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  border-radius: 15px;
+
+
+  span {
+    display: flex;
+    align-items: baseline;
+    
+    &.button {
+      cursor: pointer;
+      padding: 2rem 0rem;
+    }
+  }
+  
+  img {
+    height: 20px;
+    width: 20px;
+    margin-right: 10px;
+  
+  }
+  
+  h2{
+    font-weight: 300;
+    color: black;
+    margin: 0;
+  }
+  
+  .newGame {
+    justify-content: center;
+    display: flex;
+  }
+  
+  .dice-container {
+    height: 50vh;
+    display: grid;
+    grid-template-rows: 1fr;
+    padding-bottom: 5rem;
+    div {
+      justify-self: center;
+      align-self: center;
+    }
+  }
+  
+  .buttonWrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .currentPoints {
+    display: flex;
+    align-self: center;
+    justify-content: center;
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.main-container::after {
+  background: #f7f7f7 ;
+  content: '';
+  z-index: -1;
+  width: 50%;
+  height: 100%;
+  top: 0;
+  right: 0;
+  display: block;
+  position: absolute;
+  border-radius: 15px;
 }
 </style>
